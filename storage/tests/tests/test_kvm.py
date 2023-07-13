71ed72fa70cc3327c17cd422867a5038dbc4862f
Bartlomiej Kuczma <bartlomiejx.kuczma@intel.com>
2023-07-13 13:41:47 +0200

    test opi_env

--- storage/tests/tests/test_kvm.py
+++ storage/tests/tests/test_kvm.py
@@ -207,7 +207,7 @@ class TestOPIenv(BaseTest):
     def setUp(self):
         self.ssh_terminal = SSHTerminal(IPUStorageConfig())
         self.clone_requirements = \
-            F"""git clone https://github.com/spdk/spdk --recursive && """
+            F"""git clone https://github.com/spdk/spdk --recursive"""
         # F"""git clone https://github.com/opiproject/opi-api && """
         # F"""git clone https://github.com/opiproject/opi-intel-bridge && """
         # F"""git clone https://github.com/opiproject/opi-spdk-bridge && """
